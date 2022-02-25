ad27e7252d8511453cd3fb3c8364eda6eea1da86
Skandaniel92 <dani32c4@stud.kea.dk>
2022-02-25 13:29:15 +0100

    ny agemetode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
+    Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         Main obj = new Main();
         System.out.println("It works!");
         obj.greet();
+
+        int age = obj.askAboutAge(0);
+
+        System.out.println(age);
+
     }
 
     public void greet(){
-        Scanner input = new Scanner(System.in);
+
         System.out.println("Indtast dit navn --> ");
         String name = input.nextLine();
 
-        System.out.println(name);
+        System.out.println("Hej navn " + name);
+    }
 
-    };
+    public int askAboutAge(int age){
+
+        System.out.println("Indtast din alder --> ");
+        age = input.nextInt();
+
+        return age;
+
+    }
 }
