e4be006e9801ffd8d50b026c16e27c8e8d77f0c9
Skandaniel92 <dani32c4@stud.kea.dk>
2022-02-25 13:14:52 +0100

    added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Main obj = new Main();
         System.out.println("It works!");
+        obj.greet();
     }
+
+    public void greet(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Indtast dit navn --> ");
+        String name = input.nextLine();
+
+        System.out.println(name);
+
+    };
 }
